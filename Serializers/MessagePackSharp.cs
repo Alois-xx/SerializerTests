c373120079d92b6a98356556fddf1e542a546b0b
neuecc <ils@neue.cc>
2017-08-07 19:24:52 +0900

    add MessagePack-Sharp

---
+++ Serializers/MessagePackSharp.cs
@@ -0,0 +1,29 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializerTests.Serializers
+{
+    public class MessagePackSharp<T> : TestBase<T, MessagePack.IFormatterResolver> where T : class
+    {
+        public MessagePackSharp(Func<int, T> testData)
+        {
+            base.CreateNTestData = testData;
+            FormatterFactory = () => MessagePack.Resolvers.StandardResolver.Instance;
+        }
+
+        protected override void Serialize(T obj, Stream stream)
+        {
+            MessagePackSerializer.Serialize(stream, obj, Formatter);
+        }
+
+        protected override T Deserialize(Stream stream)
+        {
+            return MessagePackSerializer.Deserialize<T>(stream, Formatter);
+        }
+    }
+}
--- TypesToSerialize/BookShelf.cs
+++ TypesToSerialize/BookShelf.cs
@@ -1,4 +1,5 @@
-﻿using ProtoBuf;
+﻿using MessagePack;
+using ProtoBuf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +9,17 @@ using System.Threading.Tasks;
 
 namespace SerializerTests.TypesToSerialize
 {
-    [Serializable, DataContract, ProtoContract]
+    [Serializable, DataContract, ProtoContract, MessagePackObject]
     public class BookShelf
     {
-        [DataMember, ProtoMember(1)]
+        [DataMember, ProtoMember(1), Key(0)]
         public List<Book> Books
         {
             get;
             set;
         }
 
-        [DataMember, ProtoMember(2)]
+        [DataMember, ProtoMember(2), Key(1)]
         private string Secret;
 
         public BookShelf(string secret)
@@ -30,13 +31,13 @@ namespace SerializerTests.TypesToSerialize
         { }
     }
 
-    [Serializable,DataContract,ProtoContract]
+    [Serializable, DataContract, ProtoContract, MessagePackObject]
     public class Book
     {
-        [DataMember, ProtoMember(1)]
+        [DataMember, ProtoMember(1), Key(0)]
         public string Title;
 
-        [DataMember, ProtoMember(2)]
+        [DataMember, ProtoMember(2), Key(1)]
         public int Id;
     }
 

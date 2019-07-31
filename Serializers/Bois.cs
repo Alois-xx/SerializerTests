7513cbce484dea9c0a09ee8dfb4dc6f6e5ae9aa8
salarcode <salar2k@gmail.com>
2019-08-01 09:55:07 +1000

    Salar.Bois.LZ4 benchmark is added

--- Serializers/Bois.cs
+++ Serializers/Bois.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Salar.Bois;
 using System.Runtime.CompilerServices;
+using Salar.Bois.LZ4;
 
 namespace SerializerTests.Serializers
 {
@@ -34,4 +35,31 @@ namespace SerializerTests.Serializers
             return Formatter.Deserialize<T>(stream);
         }
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [SerializerType("https://github.com/salarcode/Bois",
+	    SerializerTypes.Binary)]
+    class Bois_LZ4<T> : TestBase<T, BoisSerializer>
+    {
+	    new BoisLz4Serializer Formatter = new BoisLz4Serializer();
+	    public Bois_LZ4(Func<int, T> testData, Action<T> toucher) : base(testData, toucher)
+	    {
+	    }
+
+	    [MethodImpl(MethodImplOptions.NoInlining)]
+	    protected override void Serialize(T obj, Stream stream)
+	    {
+		    Formatter.Pickle(obj, stream);
+	    }
+
+	    [MethodImpl(MethodImplOptions.NoInlining)]
+	    protected override T Deserialize(Stream stream)
+	    {
+		    return Formatter.Unpickle<T>(stream);
+	    }
+    }
+
 }

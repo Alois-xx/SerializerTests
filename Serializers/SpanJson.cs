a4eec7f88864e96f180a81a37150e0a0090210af
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2019-10-20 19:30:52 +0200

    Add SpanJson and Swifter.Json

---
+++ Serializers/SpanJson.cs
@@ -0,0 +1,33 @@
+﻿#if NETCOREAPP3_0
+
+namespace SerializerTests.Serializers
+{
+    using System;
+    using System.IO;
+    using System.Runtime.CompilerServices;
+    using SpanJson;
+    using SpanJson.Resolvers;
+
+    [SerializerType("https://github.com/Tornhoof/SpanJson",
+                    SerializerTypes.Json)]
+    class SpanJson<T> : TestBase<T, SpanJsonOptions>
+    {
+        public SpanJson(Func<int, T> testData, Action<T> dataToucher) : base(testData, dataToucher)
+        {
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        protected override void Serialize(T obj, Stream stream)
+        {
+            JsonSerializer.Generic.Utf8.SerializeAsync(obj, stream);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        protected override T Deserialize(Stream stream)
+        {
+            return JsonSerializer.Generic.Utf8.DeserializeAsync<T>(stream).Result;
+        }
+    }
+}
+
+#endif
